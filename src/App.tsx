5906270ba79d9f8fa01b306a3df9d98a119c74c4
migbash <20924663+migbash@users.noreply.github.com>
2020-11-13 01:58:43 +0000

    Updating repo fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,96 +3,45 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import { ImageCropFeedback } from './components/ImageCropFeedback';
 
-var height = 0, width = 0, imgSrc = 'https://placekitten.com/600/600'
-var dataURL = ''
-var crop = false
+var imgSrc = 'https://placekitten.com/600/600', dataURL = ''
 
 function App() {
 
-  console.log('Component Re-render')
+  const [dimension, setDimension] = useState([120, 500, 353, 403])
 
-  // ~~~~~~~~~~~~~~~~
-  // Using React Hooks as State for the component
-
-  const [dimension, setDimension] = useState([100, 100, 100, 100])
-  const [clickCount, setClickCount] = useState(0)
-  const [clickOne, setClickOne] = useState([0])
-  const [clickTwo, setClickTwo] = useState([0])
-
-  console.log(dimension)
-
-  // ~~~~~~~~~~~~~~~~
-  // Component Functions
-
-  const newAreaSelect = (top: number, left: number, right: number, bottom: number) => {
-    // RESET THE VALUES OF THE RECTANLGE TO (0, 0, 0, 0)
+  const onAreaSelect = (top: number, left: number, right: number, bottom: number) => {
     setDimension([top, left, right, bottom])
-    setClickCount(1)
   }
 
-  const tmpCanvasHandler = () => {
+  useEffect(() => {
+    tmpCanvasHandler()
+  }, [dimension])
 
+  const tmpCanvasHandler = () => {
     var canvas:any = document.getElementById('canvas_tmp')
     canvas.style = `position: absolute;
                     display: none; `
     var ctx = canvas.getContext("2d");
-    
     var img = new Image();
     img.src = imgSrc
     img.crossOrigin = '*';
     img.onload = function() {
-
-      canvas.width = img.width;
-      canvas.height = img.height;
-
-      ctx.drawImage(img, dimension[0], dimension[1], dimension[3], dimension[2], 0, 0, 250, 250);  // draw the image on the canvas
-
+      canvas.width = 250;
+      canvas.height = 250;
+      const height = dimension[2] - dimension[0]
+      const width = dimension[3] - dimension[1]
+      ctx.drawImage(img, dimension[0], dimension[1], height, width, 0, 0, 250, 250);  // img, offsetX, offsetY, sWidth, sHeight
       // update the image src:
       var t_img:HTMLImageElement|any = document.getElementById('target_img')
       dataURL = canvas.toDataURL();
       t_img.src = dataURL
-
-      crop = false
     }
     ctx.restore()
   }
-
-  // FUNCTION CLICK COMPONENT CLICK EVENT (1st & 2nd Clicks)
-  onclick = function(e) {
-    if (clickCount === 1) {
-      setClickOne([e.offsetX, e.offsetY])
-      setClickCount(clickCount + 1)
-    } 
-    
-    if (clickCount === 2) {
-      setClickTwo([e.offsetX, e.offsetY])
-      setClickCount(0)
-    }
-  };
-
-  // USE-EFFECT [2nd Click Trigger]
-  useEffect(() => {
-    // prevent intial render execution, wait for intial clicks
-    if (clickCount !== 0) {
-      width = clickTwo[0] - clickOne[0]
-      height = clickTwo[1] - clickOne[1]
-      setDimension([clickOne[0], clickOne[1], height, width])
-      crop = true // Notify of updated rectangle crop
-    }
-  }, [clickTwo])
-
-  // USE-EFFECT [Crop Rect-Upadate]
-  useEffect(() => {
-    if(crop) {
-      tmpCanvasHandler()
-    }
-  }, [crop])
-
-  // ~~~~~~~~~~~~
   
   return (
     <div className="App">
-      <ImageCropFeedback imageUrl={imgSrc} top={dimension[0]} left={dimension[1]} right={dimension[2]} bottom={dimension[3]} onAreaSelect={newAreaSelect} />
+      <ImageCropFeedback imageUrl={imgSrc} top={dimension[0]} left={dimension[1]} right={dimension[2]} bottom={dimension[3]} onAreaSelect={onAreaSelect} />
       <canvas id='canvas_tmp'> </canvas>
       <p> Result: </p>
       <img alt='cropped_img' src={dataURL} id='target_img'></img>
--- src/components/ImageCropFeedback.tsx
+++ src/components/ImageCropFeedback.tsx
@@ -1,7 +1,4 @@
-import React, { useEffect } from "react";
-
-// ~~~~~~~~~~~~~~
-// Component Interface
+import React, { useEffect, useState } from "react";
 
 interface ImageCropFeedbackProps {
     imageUrl: string;
@@ -9,7 +6,6 @@ interface ImageCropFeedbackProps {
     left: number;
     right: number;
     bottom: number;
-
     onAreaSelect: (
         top: number,
         left: number,
@@ -18,43 +14,52 @@ interface ImageCropFeedbackProps {
     ) => void;
 }
 
-// ~~~~~~~~~~~~~~~~~
-// TypeScript Function Component
-
 export const ImageCropFeedback: React.FC<ImageCropFeedbackProps> = ({ imageUrl, top, left, right, bottom, onAreaSelect }) => {
 
-    // USE-EFFECT [2nd Click Trigger]
+    const [clickCount, setClickCount] = useState(0)
+    const [clickOne, setClickOne] = useState([0])
+    const [clickTwo, setClickTwo] = useState([0])
+
+    const handleClick = (e:any) => {
+        if (clickCount === 0) {
+            setClickOne([e.nativeEvent.offsetX, e.nativeEvent.offsetY])
+            setClickCount(1)
+        } 
+        if (clickCount === 1) {
+            setClickTwo([e.nativeEvent.offsetX, e.nativeEvent.offsetY])
+            setClickCount(0)
+        }
+    }
+
     useEffect(() => {
+        if(clickTwo.length !== 1) {
+            onAreaSelect(clickOne[0], clickOne[1], clickTwo[0], clickTwo[1])
+        }
+    }, [clickTwo])
 
+    useEffect(() => {
         var c : any = document.getElementById("canvas_t");
-
         var img = new Image();
         img.src = imageUrl
         img.crossOrigin = '*';
         img.onload = function() {
-
             var ctx = c.getContext("2d");
-
             ctx.width = 200
             ctx.height = 300
-
             c.width = img.width;
             c.height = img.height;
-
+            const height = right - top
+            const width = bottom - left
             ctx.drawImage(img, 0, 0); // image draw on canvas
-
-            // rectangle draw
             ctx.beginPath();
-            ctx.rect(top, left, bottom, right); // x, y coordinate of the top left corner + width & height dimensions
+            ctx.rect(top, left, height, width); // x, y, height, width
             ctx.stroke();
         }
     })
 
-    // ~~~~~~~~~~~~~~~~~~
-
     return (
         <div>  
-            <canvas id='canvas_t' onDoubleClick={() => onAreaSelect(0, 0, 0, 0)}> </canvas>
+            <canvas id='canvas_t' onClick={(e) => handleClick(e)}> </canvas>
         </div>
     );
 };
